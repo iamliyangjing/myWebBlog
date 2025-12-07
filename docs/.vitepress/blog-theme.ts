db492badbb85a02c67180a57ba978164b70c856d
Cooper <958390434@qq.com>
2025-12-07 14:52:30 +0800

    feat: 修改相关config

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -76,39 +76,39 @@ const blogTheme = getThemeConfig({
   ],
 
   // 公告
-  popover: {
-    title: '公告',
-    body: [
-      { type: 'text', content: '👇公众号👇---👇 微信 👇' },
-      {
-        type: 'image',
-        src: 'https://img.cdn.sugarat.top/mdImg/MTYxNTAxODc2NTIxMA==615018765210~fmt.webp'
-      },
-      {
-        type: 'text',
-        content: '欢迎大家加群&私信交流'
-      },
-      {
-        type: 'text',
-        content: '文章首/文尾有群二维码',
-        style: 'padding-top:0'
-      },
-      {
-        type: 'button',
-        content: '作者博客',
-        link: 'https://sugarat.top'
-      },
-      {
-        type: 'button',
-        content: '加群交流',
-        props: {
-          type: 'success'
-        },
-        link: 'https://theme.sugarat.top/group.html',
-      }
-    ],
-    duration: 0
-  },
+  // popover: {
+  //   title: '公告',
+  //   body: [
+  //     { type: 'text', content: '👇公众号👇---👇 微信 👇' },
+  //     {
+  //       type: 'image',
+  //       src: 'https://img.cdn.sugarat.top/mdImg/MTYxNTAxODc2NTIxMA==615018765210~fmt.webp'
+  //     },
+  //     {
+  //       type: 'text',
+  //       content: '欢迎大家加群&私信交流'
+  //     },
+  //     {
+  //       type: 'text',
+  //       content: '文章首/文尾有群二维码',
+  //       style: 'padding-top:0'
+  //     },
+  //     {
+  //       type: 'button',
+  //       content: '作者博客',
+  //       link: 'https://sugarat.top'
+  //     },
+  //     {
+  //       type: 'button',
+  //       content: '加群交流',
+  //       props: {
+  //         type: 'success'
+  //       },
+  //       link: 'https://theme.sugarat.top/group.html',
+  //     }
+  //   ],
+  //   duration: 0
+  // },
 })
 
 export { blogTheme }
